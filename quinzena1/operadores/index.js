99109f39c4e3cf8c47f01a511533cea89c2f8d9e
Israel <israelcarlos@hotmail.com>
2021-05-25 00:18:18 -0300

    Exercícios Operadores e Lógica de Programação

---
+++ quinzena1/operadores/index.js
@@ -0,0 +1,51 @@
+/*  ------------- Exercícios de interpretação de código ---------------
+
+-- 1 --.
+
+a. false
+b. false
+c. true
+d. Boolean
+
+-- 2 --
+
+primeiroNumerosegundoNumero
+
+-- 3 --
+
+Converter as strings para Número, pois a operação : 
+let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!")) */
+
+
+
+// ------------- Exercícios de escrita de código ---------------
+console.log ("--Hello Labenu--")
+
+// -- 1 --
+
+// const idade = Number(prompt ("Qual a sua idade?"))
+// const Bestfriend = Number(prompt ("Qual a idade do seu melhor Amigo(a)"))
+
+// console.log ("Sua idade é maior do que a do seu melhor amigo?", idade>Bestfriend)
+
+// const diferença = idade - Bestfriend
+// console.log ("A diferença de idade é de", diferença, "anos")
+
+// -- 2 --
+
+//a 
+/* const a = Number(prompt ("Insira um número par"))
+b = 2 */
+//b 
+/* const Divisão = a % b
+console.log ("Resto da divisão é", Divisão) // O resto dos números pares sempre é igual a 0 */
+
+//c 
+// Resto dos números impares sempre são 1
+
+// -- 3 --
+
+
+
+
