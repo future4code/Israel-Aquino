3664208d65b7f0ee5d882491e3686ed6fa3e80ed
Israel <israelcarlos@hotmail.com>
2021-05-25 22:23:25 -0300

    correção da última questão

--- quinzena1/operadores/index.js
+++ quinzena1/operadores/index.js
@@ -58,9 +58,9 @@ console.log ("Você tem", idadeHoras, "horas na terra.")
 
 // -- 4 --
 let numero1 = Number(prompt("digite um número"))
-let numero2 = Number(prompt("agora digita outro númro ai")) 
+let numero2 = Number(prompt("agora digita outro número ai")) 
 
 console.log("O primeiro número é maior que o segundo?", numero1 > numero2)
 console.log("O primeiro número é igual ao segundo", numero1 == numero2)
-console.log("O primeiro numero é divisível pelo segundo?", numero1 % numero2)
-console.log("O segundo numero é divisível pelo primeiro?", numero2 % numero1)
+console.log("O primeiro número é divisível pelo segundo?", (numero1 % numero2) == 0)
+console.log("O segundo numero é divisível pelo primeiro?", (numero2 % numero1) ==0)
