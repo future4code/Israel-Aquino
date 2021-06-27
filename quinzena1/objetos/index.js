594d1b7be73cfdec70d775bc08aa1db6ca2346f8
Israel <israelcarlos@hotmail.com>
2021-06-27 10:45:19 -0300

    Atividade Objetos que faltou entrega

--- quinzena1/objetos/index.js
+++ quinzena1/objetos/index.js
@@ -43,70 +43,70 @@
 
 // 3)
 // a - 
-let carrinho = []
+// let carrinho = []
 
-// // b -
-const frutaUm = {
-  nome: "Banana", disponivel:true 
-};
-const frutaDois = {
-  nome: "Maçã", disponivel:true 
-};
-const frutaTres = {
-  nome: "Melão", disponivel:true 
-}
-
-// // c - 
-function fruta(input1, input2, input3) {
-carrinho.push(input1, input2, input3)
-console.log(carrinho)
-}
-
-// // d- 
-fruta(frutaUm,frutaDois,frutaTres)
-
-// ------- Desafio --------------
-// 1)
-
-// const nome = prompt("Qual é o seu nome?")
-// const idade = Number(prompt("Qual é a sua idade"))
-// const job = prompt("Qual é a sua profissão?")
-
-// function inputData (input1, input2, input3) {
-//   const inputFinal = {
-//     nome: input1,
-//     idade:input2,
-//     profissão: input3
-//   }
-//   console.log(inputFinal)
-//   console.log(typeof inputFinal)
+// // // b -
+// const frutaUm = {
+//   nome: "Banana", disponivel:true 
+// };
+// const frutaDois = {
+//   nome: "Maçã", disponivel:true 
+// };
+// const frutaTres = {
+//   nome: "Melão", disponivel:true 
 // }
-// inputData(nome,idade,job)
 
-// 2)
-// function movies(){
-//   const filmeUm ={
-//     nome: "Harry Potter e a Pedra Filosofal",
-//     "Ano Lançamento": 2001
-//   };
-//   const filmeDois = {
-//     nome: "O Senhor dos Anéis: As Duas Torres",
-//     "Ano Lançamento": 2002
-//   }
-//   console.log("Primeiro Filme:", filmeUm.nome +'.'+ "\nAno de lançamento:",filmeUm["Ano Lançamento"])
-//   console.log("Primeiro Filme:", filmeDois.nome +'.'+ "\nAno de lançamento:", filmeDois["Ano Lançamento"])
-//   console.log("O primeiro filme foi lançado antes do segundo?", filmeUm["Ano Lançamento"] < filmeDois["Ano Lançamento"])
-//   console.log("O primeiro filme foi lançado no mesmo ano do segundo?", filmeUm["Ano Lançamento"] === filmeDois["Ano Lançamento"])
+// // // c - 
+// function fruta(input1, input2, input3) {
+// carrinho.push(input1, input2, input3)
+// console.log(carrinho)
 // }
-// movies()
 
-// 3)
-function inputDisponibilidade(fruit){
-const disponibilidade ={
-  ...fruit,
-  disponivel: !fruit.disponivel
-}
-return disponibilidade
-}
+// // // d- 
+// fruta(frutaUm,frutaDois,frutaTres)
 
-console.log(inputDisponibilidade(frutaUm))
+// // ------- Desafio --------------
+// // 1)
+
+// // const nome = prompt("Qual é o seu nome?")
+// // const idade = Number(prompt("Qual é a sua idade"))
+// // const job = prompt("Qual é a sua profissão?")
+
+// // function inputData (input1, input2, input3) {
+// //   const inputFinal = {
+// //     nome: input1,
+// //     idade:input2,
+// //     profissão: input3
+// //   }
+// //   console.log(inputFinal)
+// //   console.log(typeof inputFinal)
+// // }
+// // inputData(nome,idade,job)
+
+// // 2)
+// // function movies(){
+// //   const filmeUm ={
+// //     nome: "Harry Potter e a Pedra Filosofal",
+// //     "Ano Lançamento": 2001
+// //   };
+// //   const filmeDois = {
+// //     nome: "O Senhor dos Anéis: As Duas Torres",
+// //     "Ano Lançamento": 2002
+// //   }
+// //   console.log("Primeiro Filme:", filmeUm.nome +'.'+ "\nAno de lançamento:",filmeUm["Ano Lançamento"])
+// //   console.log("Primeiro Filme:", filmeDois.nome +'.'+ "\nAno de lançamento:", filmeDois["Ano Lançamento"])
+// //   console.log("O primeiro filme foi lançado antes do segundo?", filmeUm["Ano Lançamento"] < filmeDois["Ano Lançamento"])
+// //   console.log("O primeiro filme foi lançado no mesmo ano do segundo?", filmeUm["Ano Lançamento"] === filmeDois["Ano Lançamento"])
+// // }
+// // movies()
+
+// // 3)
+// function inputDisponibilidade(fruit){
+// const disponibilidade ={
+//   ...fruit,
+//   disponivel: !fruit.disponivel
+// }
+// return disponibilidade
+// }
+
+// console.log(inputDisponibilidade(frutaUm))
